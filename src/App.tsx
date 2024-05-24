2e3f011dfc29afa41e705376acfa23877b5bc194
LautaroZalazar <lautyzalazar10@gmail.com>
2024-05-23 21:50:05 -0300

    delete login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Auth } from "./pages";
-import Login from "./pages/auth/login/Login";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/auth/login" element={<Login />} />
           <Route path="/auth" element={<Auth />} />
         </Routes>
       </Router>
